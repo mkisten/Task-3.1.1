6a43e1df5d82bd55648c5fef1ddbac908d7a90d9
mkisten <126315397+mkisten@users.noreply.github.com>
2023-07-26 18:43:23 +0300

    Task-3.1.1

---
+++ Task-7/src/main/java/web/services/UsersServiceImp.java
@@ -0,0 +1,51 @@
+package web.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.models.User;
+
+import java.util.List;
+
+    @Service
+    public class UsersServiceImp implements UsersService {
+        UserDao userDao;
+
+        @Autowired
+        public UsersServiceImp(UserDao userDao) {
+            this.userDao = userDao;
+        }
+
+        @Override
+        public List<User> getAllUsers() {
+            return userDao.getAllUsers();
+        }
+
+        @Transactional
+        @Override
+        public void saveUser(User user) {
+            userDao.saveUser(user);
+
+        }
+
+        @Transactional
+        @Override
+        public void deleteUser(Long id) {
+            userDao.deleteUser(id);
+
+        }
+
+        @Override
+        public User getUserById(Long id) {
+            return userDao.getUserById(id);
+        }
+
+        @Transactional
+        @Override
+        public void updateUser(Long id, User updatedUser) {
+            userDao.updateUser(id, updatedUser);
+
+        }
+    }
+
